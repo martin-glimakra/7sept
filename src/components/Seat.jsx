1785938407f2c8fb960e938ec403bd2a2916f16f
martin-glimakra <88892064+martin-glimakra@users.noreply.github.com>
2022-09-08 14:33:31 +0200

    toggle

--- src/components/Seat.jsx
+++ src/components/Seat.jsx
@@ -4,10 +4,29 @@ export default function Seat({ seat, marked, setMarked }) {
     let isMarked = marked.filter(stefan => stefan === seat.id)
 
 
+    function handleClick (id) {
+        if (marked.filter(number => number === id).length) {
+            console.log('if')
+            let newState = marked.filter(remove => remove != id)
+            setMarked(newState)
+
+            
+        }
+        else {
+            console.log('else')
+            setMarked([...marked, id])
+
+        }
+    } 
+
+    console.log(marked)
+
+
+
     return (
         <div className={isMarked.length ? 'marked' : 'not-marked'}>
             <h3>{seat.name}</h3>
-            {seat.booked ? <div>är bokad</div> : <button onClick={() => setMarked([...marked, seat.id])}>markera</button>}
+            {seat.booked ? <div>är bokad</div> : <button onClick={() => handleClick(seat.id)}>markera</button>}
         </div>
     )
 }
